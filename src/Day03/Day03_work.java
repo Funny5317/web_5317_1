bd92dfa8e36debc0f076c5a2341d11ee034612ba
82105 <82105@121.170.0.129>
2021-10-02 16:58:24 +0900

    java04.

---
+++ src/Day03/Day03_work.java
@@ -0,0 +1,11 @@
+package Day03;
+
+public class Day03_work {
+	
+	public static void main(String[] args) {
+		
+		
+		
+	}
+	
+}
---
+++ src/Z/exercisebook.java
@@ -0,0 +1,18 @@
+package Z;
+
+import java.util.Scanner;
+
+public class exercisebook {
+
+	public static void main(String[] args) {
+		
+		/*
+		 * 입력받은 값을 화면에 출력
+		 */
+		
+		System.out.println("넣을 소수");
+	
+	}
+
+	
+}
